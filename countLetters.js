ccd72d36ae0456d786c995dca59cd384534a9bd1
daniscodes <dan97@bell.net>
2022-07-27 17:09:40 -0400

    Created countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,25 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} ===  ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
+  }
+};
+
+const countLetters = function(string) {
+  const output = {}
+
+  for (const letter of string){
+    if (letter !== ""){
+
+      if(output[letter]){
+        output[letter] += 1;
+      } else {
+        output[letter] = 1;
+      }
+    }
+  }
+  return output
+}
+
+console.log(countLetters("lighthouse in the house"))
