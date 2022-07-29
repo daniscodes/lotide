916e8304dd28f1fe0de299accae8705e0c36534e
daniscodes <dan97@bell.net>
2022-07-28 23:48:07 -0400

    Create assertObjectsEqual.js

---
+++ assertObjectsEqual.js
@@ -0,0 +1,40 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} ===  ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
+  }
+
+};
+
+const eqArrays = function(arr1, arr2){
+if (JSON.stringify(arr1) === JSON.stringify(arr2)){
+  return true
+} else return false
+}
+
+const eqObjects = function(object1, object2){
+  if (Object.keys(object1).length === Object.keys(object2).length){
+    for(let key of Object.keys(object1) ){
+        if(Array.isArray(object1[key]) && Array.isArray(object2[key])){
+            if (!eqArrays(object1[key],object2[key])){
+             return false;       
+            }
+        }
+        
+    }
+    return true;
+}
+return false;    
+};
+
+// FUNCTION IMPLEMENTATION
+const assertObjectsEqual = (actual, expected) => {
+  // import util library to use inspect function
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`⛔️⛔️⛔️ Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
