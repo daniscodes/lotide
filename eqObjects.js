80f5162f77e6e825fe42ee8d6d2d312882901765
daniscodes <dan97@bell.net>
2022-07-28 22:00:12 -0400

    Create eqObjects.js

---
+++ eqObjects.js
@@ -0,0 +1,29 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} ===  ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
+  }
+
+};
+
+const eqArrays = function(arr1, arr2){
+if (JSON.stringify(arr1) === JSON.stringify(arr2)){
+  return true
+} else return false
+}
+
+const eqObjects = function(object1, object2){
+  if (Object.keys(object1).length === Object.keys(object2).length){
+    for(let key of Object.keys(object1) ){
+        if(Array.isArray(object1[key]) && Array.isArray(object2[key])){
+            if (!eqArrays(object1[key],object2[key])){
+             return false;       
+            }
+        }
+        
+    }
+    return true;
+}
+return false;    
+};
