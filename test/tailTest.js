bf1b8778a6527ff7f50d2f681c69cace341dcea0
daniscodes <dan97@bell.net>
2022-08-02 00:07:48 -0400

    refactor tailTest to M&C

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,22 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require("../tail");
 
 
+// // Test Case 1: Check the returned array elements
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result.length, 2); // ensure we get back two elements
+// assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+// assertEqual(result[1], "Labs"); // ensure second element is "Labs"
 
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+describe("#tail", () => {
+  const result = ["Hello", "Lighthouse", "Labs"];
+  it("returns 3 for result.length", () => {
+    assert.strictEqual((result.length), 3);
+  });
+  it("returns 2 for tail(result).length", () => {
+    assert.strictEqual(tail(result).length, 2);
+  });
+  it("returns ['Lighthouse', 'Labs'] for tail(result)", () => {
+    assert.deepEqual(tail(result), ['Lighthouse', 'Labs']);
+  });
+});
