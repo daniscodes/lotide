ec106bbb427b80a522edaa7f525eff7c33afdc39
daniscodes <dan97@bell.net>
2022-08-01 23:53:44 -0400

    refactored middle

--- middle.js
+++ middle.js
@@ -8,9 +8,4 @@ const middle = function(array){
   }
 };
 
-console.log(middle([1]))
-console.log(middle([1, 2]))
-console.log(middle([1, 2, 3]))
-console.log(middle([1, 2, 3, 4]))
-console.log(middle([1, 2, 3, 4, 5]))
-console.log(middle([1, 2, 3, 4, 5, 6]))
+module.exports = middle;
---
+++ test/middleTest.js
@@ -0,0 +1,8 @@
+const middle = require("../middle");
+
+console.log(middle([1]))
+console.log(middle([1, 2]))
+console.log(middle([1, 2, 3]))
+console.log(middle([1, 2, 3, 4]))
+console.log(middle([1, 2, 3, 4, 5]))
+console.log(middle([1, 2, 3, 4, 5, 6]))
